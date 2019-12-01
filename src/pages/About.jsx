aa1cc7895ab0be91ed12eb3e26c13e98a6e4785a
XX <fallwall19@gmail.com>
2019-11-30 23:58:55 -0500

    bio.techstack.animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import data from '../data/about_data.json';
+import './about.css';
 
-const About=()=> {
+const About = () => {
   return (
-    <div>
-      {data.bio}
-      {data.frontStack}
-      {data.backStack}
+    <div className="bio">
+      <div className="bio-wrapper">
+        <p>{data.bio}</p>
+      </div>
+      <div className="tech-stack-wrapper">
+        <div className="tech-stack-front-wrapper">
+          {data.frontStack.map(i => <div className="tech-stack tech-stack-front" key={i}>{i}</div>)}
+        </div>
+        <div className="tech-stack-back-wrapper">
+          {data.backStack.map(i => <div className="tech-stack tech-stack-back" key={i}>{i}</div>)}
+        </div>
+      </div>
     </div>
   )
 }
