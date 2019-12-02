9b033294876b245435b062f3a5606c8b6a868f9a
XX <fallwall19@gmail.com>
2019-12-02 12:20:59 -0500

    contact social

---
+++ src/components/ContactSocial.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const ContactSocial = () => {
+  return (
+    <div classNameName="contact-social">
+      <div className="ui list">
+        <div className="item">
+          <i className="marker icon huge"></i>
+          <div className="content">
+            New York, NY
+    </div>
+        </div>
+        <div className="item">
+          <i className="mail icon huge"></i>
+          <div className="content">
+            <a href="mailto:sherunsfreely@gmail.com">sherunsfreely@gmail.com</a>
+          </div>
+        </div>
+        <div className="item">
+          <i className="github icon huge"></i>
+          <div className="content">
+            <a href="https://github.com/fallwall">@fallwall</a>
+          </div>
+        </div>
+        <div className="item">
+          <i className="instagram icon huge"></i>
+          <div className="content">
+            <a href="http://www.instagram.com/fallwall19">@fallwall19</a>
+          </div>
+        </div>
+        <div className="item">
+          <i className="linkedin icon huge"></i>
+          <div className="content">
+            <a href="https://www.linkedin.com/in/tibbyxu/">@TibbyXu</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContactSocial;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ContactSocial from '../components/ContactSocial';
 
-const Contact=()=> {
+const Contact = () => {
   return (
     <div className="page-frame">
-      
+      <ContactSocial />
     </div>
   )
 }
