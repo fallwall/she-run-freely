fb7d3bd4263b3652b6e25e709ee6f7217b37d8f5
XX <fallwall19@gmail.com>
2019-12-02 12:41:27 -0500

    social icon margin'

--- src/components/ContactSocial.jsx
+++ src/components/ContactSocial.jsx
@@ -4,35 +4,39 @@ const ContactSocial = () => {
   return (
     <div className="contact-social">
       <div className="ui horizontal list">
-
         <div className="item">
-          <i className="mail icon huge"></i>
           <div className="content">
-            <a href="mailto:sherunsfreely@gmail.com" target="_blank"></a>
+            <a href="mailto:sherunsfreely@gmail.com" target="_blank">
+              <i className="mail icon huge"></i>
+            </a>
           </div>
         </div>
         <div className="item">
-          <i className="github icon huge"></i>
           <div className="content">
-            <a href="https://github.com/fallwall" target="_blank"></a>
+            <a href="https://github.com/fallwall" target="_blank">
+              <i className="github icon huge"></i>
+            </a>
           </div>
         </div>
         <div className="item">
-          <i className="instagram icon huge"></i>
           <div className="content">
-            <a href="http://www.instagram.com/fallwall19" target="_blank"></a>
+            <a href="http://www.instagram.com/fallwall19" target="_blank">
+              <i className="instagram icon huge"></i>
+            </a>
           </div>
         </div>
         <div className="item">
-          <i className="linkedin icon huge"></i>
           <div className="content">
-            <a href="https://www.linkedin.com/in/tibbyxu/" target="_blank"></a>
+            <a href="https://www.linkedin.com/in/tibbyxu/" target="_blank">
+              <i className="linkedin icon huge"></i>
+            </a>
           </div>
         </div>
         <div className="item">
-          <i className="strava icon huge"></i>
           <div className="content">
-            <a href="https://www.strava.com/athletes/18133452" target="_blank"></a>
+            <a href="https://www.strava.com/athletes/18133452" target="_blank">
+              <i className="strava icon huge"></i>
+            </a>
           </div>
         </div>
       </div>
