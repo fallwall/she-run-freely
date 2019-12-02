2d6e8b69aafe28c5e44cbbf2fcb62ae8af590514
XX <fallwall19@gmail.com>
2019-12-02 12:29:50 -0500

    social link horizontal

--- src/components/ContactSocial.jsx
+++ src/components/ContactSocial.jsx
@@ -3,35 +3,36 @@ import React from 'react';
 const ContactSocial = () => {
   return (
     <div classNameName="contact-social">
-      <div className="ui list">
-        <div className="item">
-          <i className="marker icon huge"></i>
-          <div className="content">
-            New York, NY
-    </div>
-        </div>
+      <div className="ui horizontal divided list">
+        
         <div className="item">
           <i className="mail icon huge"></i>
           <div className="content">
-            <a href="mailto:sherunsfreely@gmail.com">sherunsfreely@gmail.com</a>
+            <a href="mailto:sherunsfreely@gmail.com" target="_blank">Email</a>
           </div>
         </div>
         <div className="item">
           <i className="github icon huge"></i>
           <div className="content">
-            <a href="https://github.com/fallwall">@fallwall</a>
+            <a href="https://github.com/fallwall" target="_blank">@fallwall</a>
           </div>
         </div>
         <div className="item">
           <i className="instagram icon huge"></i>
           <div className="content">
-            <a href="http://www.instagram.com/fallwall19">@fallwall19</a>
+            <a href="http://www.instagram.com/fallwall19" target="_blank">@fallwall19</a>
           </div>
         </div>
         <div className="item">
           <i className="linkedin icon huge"></i>
           <div className="content">
-            <a href="https://www.linkedin.com/in/tibbyxu/">@TibbyXu</a>
+            <a href="https://www.linkedin.com/in/tibbyxu/" target="_blank">LinkedIn</a>
+          </div>
+        </div>
+        <div className="item">
+          <i className="strava icon huge"></i>
+          <div className="content">
+            <a href="https://www.strava.com/athletes/18133452" target="_blank">Strava</a>
           </div>
         </div>
       </div>
