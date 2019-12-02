903dca3742239908763c538846408b709451cfd2
XX <fallwall19@gmail.com>
2019-12-02 12:04:59 -0500

    semantic ui cdn

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Contact() {
+const Contact=()=> {
   return (
-    <div>
+    <div className="page-frame">
       
     </div>
   )
 }
+
+export default Contact;
