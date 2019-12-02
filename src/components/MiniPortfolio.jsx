3e664aa25bfe847919bf99535efd32282ed8ef8b
XX <fallwall19@gmail.com>
2019-12-02 14:43:59 -0500

    mini port link to individual port page

--- src/components/MiniPortfolio.jsx
+++ src/components/MiniPortfolio.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import data from '../data/coding_data.json';
 
 const MiniPortfolio = () => {
   const entries = data.entries.slice(0,12);
   return (
     <div className="mini-portfolio-grid">
-      {entries.map(i => <div key={i.id} className="mini-portfolio-each">
-        <img className="mini-portfolio-img" src={i.img} />
+      {entries.map(i => <Link to={`/codesthis/${i.id}`}>
+        <div key={i.id} className="mini-portfolio-each">
+        <img className="mini-portfolio-img" src={i.img} alt="individual portfolio cover"/>
         <div className="mini-portfolio-text">{i.title}({i.year})</div>
-      </div>)}
+        </div>
+      </Link>)}
     </div>
   )
 }
