0922c7f51063f268468f20658bdc559c9b5b03d8
XX <fallwall19@gmail.com>
2019-12-02 14:24:03 -0500

    mini port grid at home

--- src/components/MiniPortfolio.jsx
+++ src/components/MiniPortfolio.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import data from '../data/coding_data.json';
 
-export default function MiniPortfolio() {
+const MiniPortfolio = () => {
+  const entries = data.entries.slice(0,12);
   return (
-    <div>
-      
+    <div className="mini-portfolio-grid">
+      {entries.map(i => <div key={i.id} className="mini-portfolio-each">
+        <img className="mini-portfolio-img" src={i.img} />
+        <div className="mini-portfolio-text">{i.title}({i.year})</div>
+      </div>)}
     </div>
   )
 }
+
+export default MiniPortfolio;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import MiniPortfolio from '../components/MiniPortfolio';
 
 const Home = () => {
   return (
     <div className="page-frame">
-
+      <MiniPortfolio />
     </div>
   )
 }
